99c7fb84e08b5249cace970d57f6d70aa836416b
吴某某 <2822653906@qq.com>
2021-12-06 00:39:10 +0800

    添加了https

--- src/api/LoginReq.js
+++ src/api/LoginReq.js
@@ -2,6 +2,6 @@ import { postReq, putReq, getReq, deleteReq } from "../api/BaseRequestApi"
 
 export const VERIFY_CODE_URL = 'http://121.40.99.230:8088/captcha.jpg?token='
 export const LoginPost = (data) => { //登入接口
-   const url= "/sys/login"
+   const url= "https://wujianqin.xyz"
 return postReq(url,data)
  }
