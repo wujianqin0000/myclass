ff234f3a486a1fe22cee3757fe997ed67d57335c
吴某某 <2822653906@qq.com>
2024-01-03 11:22:26 +0800

    更换服务器，切换api

--- src/api/AllApiUrl.js
+++ src/api/AllApiUrl.js
@@ -1,10 +1,10 @@
 // ----------------发布环境接口---------------------
 
-export const VERIFY_CODE_URL = 'https://captcha.wujianqin.xyz?token=' // https验证码接口
-export const HTTPS_LOGIN= "https://httpslogin.wujianqin.xyz" // https登录接口
-export const GEGINFOALL_BYID = "https://getinfobyid.wujianqin.xyz" // 通过id获取个人信息接口
-export const UPDATEINFOALL_BYID= "https://updateinfobyid.wujianqin.xyz" // 通过id更新个人信息接口
-export const GET_INFOLIST= "https://getallinfo.wujianqin.xyz" // 获取所有成员信息
+// export const VERIFY_CODE_URL = 'https://captcha.wujianqin.xyz?token=' // https验证码接口
+// export const HTTPS_LOGIN= "https://httpslogin.wujianqin.xyz" // https登录接口
+// export const GEGINFOALL_BYID = "https://getinfobyid.wujianqin.xyz" // 通过id获取个人信息接口
+// export const UPDATEINFOALL_BYID= "https://updateinfobyid.wujianqin.xyz" // 通过id更新个人信息接口
+// export const GET_INFOLIST= "https://getallinfo.wujianqin.xyz" // 获取所有成员信息
 
 // ----------------开发环境接口----------------------
 
@@ -13,3 +13,12 @@ export const GET_INFOLIST= "https://getallinfo.wujianqin.xyz" // 获取所有成
 // export const GEGINFOALL_BYID = "http://localhost:8088/myClassImgs/getAllById" // 通过id获取个人信息接口
 // export const UPDATEINFOALL_BYID = "http://localhost:8088/myClassImgs/updateImgInfoById" // 通过id更新个人信息接口
 // export const GET_INFOLIST= "http://localhost:8088/myClassImgs/getAll" // 获取所有成员信息
+
+
+// ----------------发布环境接口---------------------
+
+export const VERIFY_CODE_URL = 'https://myclassjava.wujianqin.xyz/myclassjava/captcha.jpg?token=' // https验证码接口
+export const HTTPS_LOGIN= "https://myclassjava.wujianqin.xyz/myclassjava/sys/login" // https登录接口
+export const GEGINFOALL_BYID = "https://myclassjava.wujianqin.xyz/myclassjava/myClassImgs/getAllById" // 通过id获取个人信息接口
+export const UPDATEINFOALL_BYID= "https://myclassjava.wujianqin.xyz/myclassjava/myClassImgs/updateImgInfoById" // 通过id更新个人信息接口
+export const GET_INFOLIST= "https://myclassjava.wujianqin.xyz/myclassjava/myClassImgs/getAll" // 获取所有成员信息
